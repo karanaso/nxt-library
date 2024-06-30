251cc8946a9afbcde79238378ac1bc6a5dd1b86c
Sotirios <sotiris@endogusto.com>
2024-06-30 19:47:17 +0300

    Make POST/PUT on EditMembers

--- src/containers/EditMember.tsx
+++ src/containers/EditMember.tsx
@@ -1,28 +1,50 @@
 import { Box, Button, TextField } from "@mui/material"
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 export const EditMember = () => {
+  const params = useParams();
+  const navigate = useNavigate();
 
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
   const [email, setEmail] = useState('');
 
-  const save = () => {
-    console.log(firstName, lastName, phoneNumber, email);
+  useEffect(() => {
+    if (params.id) load({ id: params.id })
+  }, [params.id])
 
-    fetch('http://localhost:3000/library/members', {
-      method: 'POST',
+  const load = ({id}:{id:string}) => fetch(`http://localhost:3000/library/members/${id}`)
+    .then(r => r.ok && r.json())
+    .then(d => {
+      setFirstName(d[0].firstName);
+      setLastName(d[0].lastName);
+      setPhoneNumber(d[0].phoneNumber);
+      setEmail(d[0].email);
+    })
+
+  const save = () => {
+    let url = 'http://localhost:3000/library/members';
+    let method = 'POST';
+    
+    if (params.id) {
+      url += `/${params.id}`;
+      method = 'PUT';
+    }
+
+    fetch(url, {
+      method,
       body: JSON.stringify({
         firstName,
         lastName,
         phoneNumber,
         email
       })
-    })
+    }).then(() => navigate('/members'));
   }
-  
-  const cancel = () => alert('You clicked cancel');
+
+  const cancel = () => navigate(-1);
 
   return (
     <Box
@@ -37,29 +59,29 @@ export const EditMember = () => {
       <h1>Edit Member</h1>
       <TextField
         id="outlined-basic"
-        value={firstName} 
+        value={firstName}
         label="First Name"
         variant="outlined"
         onChange={(e) => setFirstName(e.target.value)}
       />
       <TextField
         id="outlined-basic"
-        value={lastName} 
+        value={lastName}
         label="Last Name"
         variant="outlined"
         onChange={(e) => setLastName(e.target.value)}
       />
       <TextField
         id="outlined-basic"
-        value={phoneNumber} 
+        value={phoneNumber}
         label="Phone number"
         variant="outlined"
         onChange={(e) => setPhoneNumber(e.target.value)}
       />
       <TextField
         id="outlined-basic"
-        value={email} 
-        label="email" 
+        value={email}
+        label="email"
         variant="outlined"
         onChange={(e) => setEmail(e.target.value)}
       />
@@ -68,12 +90,12 @@ export const EditMember = () => {
           display: 'flex',
           flexDirection: 'row',
           gap: 1,
-          justifyContent:'end',
+          justifyContent: 'end',
 
         }}
       >
-        <Button variant="contained" color="primary" onClick={save}>Save</Button>
         <Button variant="contained" color="warning" onClick={cancel}>Cancel</Button>
+        <Button variant="contained" color="primary" onClick={save}>Save</Button>
       </Box>
     </Box>
   )

9c06d0d9752d7863ff4da969e781d8e42d7b4c9b
Sotirios <sotiris@endogusto.com>
2024-07-09 20:33:50 +0300

    Change Datatable initital page size render to 10 items

--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -17,7 +17,7 @@ export const DataTable = ({
         columns={columns}
         initialState={{
           pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
+            paginationModel: { page: 0, pageSize: 10 },
           },
         }}
         pageSizeOptions={[5, 10, 20, 50, 100]}

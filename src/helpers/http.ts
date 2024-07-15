19cb18249826a91c7856b8dce661fe1762e8b61e
Sotirios <sotiris@endogusto.com>
2024-07-15 18:44:36 +0300

    Add query to apiFactory

--- src/helpers/http.ts
+++ src/helpers/http.ts
@@ -1,27 +1,38 @@
 export const conf = {
-  booksUrl: 'http://server004.sekdev.com:1337/api/books',
-  membersUrl: 'http://server004.sekdev.com:1337/api/members',
-  transactionsUrl: 'http://server004.sekdev.com:1337/api/transactions',
+  auth: {
+    signin: 'http://localhost:1337/api/auth/local',
+  },
+  booksUrl: 'http://localhost:1337/api/books',
+  configurations: 'http://localhost:1337/api/configurations',
+  membersUrl: 'http://localhost:1337/api/members',
+  transactionsUrl: 'http://localhost:1337/api/transactions',
 }
 
 const options = {
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
-    'Authorization': 'Bearer '+localStorage.getItem('jwt')
+    'Authorization': 'Bearer ' + localStorage.getItem('jwt')
   }
 };
 
-export const apiFactory = (url:string) => ({
-  fetch: () =>
-    fetch(url, options)
-      .then(response => response.json())
-      .then(data => {
-        return data.data.map((d: any) => ({
-          id: d.id,
-          ...d.attributes
-        }));
-      }),
+export const apiFactory = (url: string) => ({
+  fetch: () => fetch(url, options)
+    .then(response => response.json())
+    .then(data => {
+      return data.data.map((d: any) => ({
+        id: d.id,
+        ...d.attributes
+      }));
+    }),
+  query: (params:string) => fetch(url+params, options)
+    .then(response => response.json())
+    .then(data => {
+      return data.data.map((d: any) => ({
+        id: d.id,
+        ...d.attributes
+      }));
+    }),
   getById: (id: string) => fetch(`${url}/${id}`, options)
     .then(response => response.json())
     .then(data => {
@@ -52,4 +63,5 @@ export const apiFactory = (url:string) => ({
 
 export const members = apiFactory(conf.membersUrl);
 export const books = apiFactory(conf.booksUrl);
-export const transactions = apiFactory(conf.transactionsUrl); 
+export const transactions = apiFactory(conf.transactionsUrl);
+export const configurations = apiFactory(conf.configurations); 
